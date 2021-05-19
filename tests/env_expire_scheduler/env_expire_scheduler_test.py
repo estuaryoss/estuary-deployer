c7003720d8ed3c818167a8a3818427edefbe2c71
Catalin Dinuta <43060213+dinuta@users.noreply.github.com>
2021-05-19 18:41:09 +0300

    enabled docker logs for debug (#16)

--- tests/env_expire_scheduler/env_expire_scheduler_test.py
+++ tests/env_expire_scheduler/env_expire_scheduler_test.py
@@ -7,6 +7,7 @@ from parameterized import parameterized
 
 class EnvExpireSchedulerTestCase(unittest.TestCase):
     server = "http://localhost:8081/docker"
+    time_deployment_up = 5
 
     @parameterized.expand([
         ("alpine.yml", "variables.yml")
@@ -14,7 +15,7 @@ class EnvExpireSchedulerTestCase(unittest.TestCase):
     def test_env_clean_up(self, template, variables):
         response = requests.post(self.server + f"/deployments/{template}/{variables}")
         self.assertEqual(response.status_code, 200)
-        time.sleep(5)
+        time.sleep(self.time_deployment_up)
         response = requests.get(self.server + "/deployments")
         self.assertEqual(len(response.json().get('description')), 1)
         time.sleep(80)
